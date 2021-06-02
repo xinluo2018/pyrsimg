cd3507ba2358569fc97b34a66dad9861e13edc3b
xin luo <xinluo_xin@163.com>
2021-06-02 16:30:57 +0800

    modify imgShow

--- utils/imgShow.py
+++ utils/imgShow.py
@@ -1,10 +1,10 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 def imgShow(img, extent=None, color_bands=(2,1,0), \
                     clip_percent=2, per_band_clip='False'):
     '''
+    update date: 2021.6.2
     Arguments:
         img: (row, col, band) or (row, col)
         num_bands: a list/tuple, [red_band,green_band,blue_band]
@@ -37,6 +37,7 @@ def imgShow(img, extent=None, color_bands=(2,1,0), \
             img_color_hist = np.percentile(img_color, [clip_percent, 100-clip_percent])
             img_color_clip = (img_color-img_color_hist[0])\
                                     /(img_color_hist[1]-img_color_hist[0]+0.0001)
+        img_color_clip = np.squeeze(img_color_clip)
         plt.imshow(np.clip(img_color_clip, 0, 1), extent=extent, vmin=0,vmax=1)
 
 
