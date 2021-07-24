8605d471a33988d7b4845415d5a6d627dc2f99e2
xin luo <xinluo_xin@163.com>
2021-07-24 09:51:35 +0800

    add raster2vec

--- utils/geotif_io.py
+++ utils/geotif_io.py
@@ -1,4 +1,4 @@
-## author: luo xin, date: 2021.6.18
+## author: luo xin, creat: 2021.6.18, modify: 2021.7.14
 
 import numpy as np
 from osgeo import gdal
@@ -42,6 +42,7 @@ def writeTiff(im_data, im_geotrans, im_geosrs, path_out):
         im_data: tow dimentions (order: row, col),or three dimentions (order: row, col, band)
         im_geosrs: espg code correspond to image spatial reference system.
     '''
+    im_data = np.squeeze(im_data)
     if 'int8' in im_data.dtype.name:
         datatype = gdal.GDT_Byte
     elif 'int16' in im_data.dtype.name:
@@ -66,41 +67,3 @@ def writeTiff(im_data, im_geotrans, im_geosrs, path_out):
         dataset.GetRasterBand(1).WriteArray(im_data)
         del dataset
 
-# import rasterio as rio
-# import numpy as np
-
-# def readTiff(path_in):
-#     ''' 
-#         author: xin luo, date: 2021.5.6
-#         input: path of the .tif image
-#         output: rasterio DatasetReader object, and np.array data (image)
-#     '''
-#     img_src = rio.open(path_in)
-#     if img_src.count > 1:
-#         bands_ind = [i for i in range(1, img_src.count+1)]
-#         img_array = img_src.read(bands_ind)
-#         img_array = img_array.transpose(1, 2, 0)
-#     else: 
-#         img_array = img_src.read(1)
-#     return img_src, img_array
-
-
-# def writeTiff(im_data, im_transform, im_crs, path_out):
-#     '''im_data: 3d (row, col, band) np.array.'''
-#     if len(im_data.shape) > 2:
-#         im_data = im_data.transpose(2,0,1)
-#         im_bands, im_height, im_width = im_data.shape
-#     else:
-#         im_bands, (im_height, im_width) = 1, im_data.shape
-#         im_data = np.expand_dims(im_data, axis=0)
-#     with rio.open(
-#         path_out, 'w',
-#         driver='GTiff',
-#         height=im_height,
-#         width=im_width,
-#         count=im_bands,
-#         dtype=im_data.dtype,
-#         crs = im_crs,
-#         transform=im_transform,
-# ) as dst:
-#         dst.write(im_data)
--- utils/imgShow.py
+++ utils/imgShow.py
@@ -1,21 +1,18 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 def imgShow(img, extent=None, color_bands=(2,1,0), \
-                    clip_percent=2, per_band_clip='False'):
+                            clip_percent=2, per_band_clip='False'):
     '''
-    update date: 2021.6.2
     Arguments:
-        img: (row, col, band) or (row, col)
+        img: (row, col, band) or (row, col), DN range should be in [0,1]
         num_bands: a list/tuple, [red_band,green_band,blue_band]
         clip_percent: for linear strech, value within the range of 0-100. 
         per_band: if 'True', the band values will be clipped by each band respectively. 
     '''
-    img = img/(np.amax(img)+0.00001)  # normalization
+    img[np.isnan(img)]=0
     img = np.squeeze(img)
-    while np.isnan(np.sum(img)) == True:
-        where_are_NaNs = np.isnan(img)
-        img[where_are_NaNs] = 0
     if np.min(img) == np.max(img):
         if len(img.shape) == 2:
             plt.imshow(np.clip(img, 0, 1), extent=extent, vmin=0,vmax=1)
@@ -29,16 +26,21 @@ def imgShow(img, extent=None, color_bands=(2,1,0), \
         img_color_clip = np.zeros_like(img_color)
         if per_band_clip == 'True':
             for i in range(img_color.shape[-1]):
-                img_color_hist = np.percentile(img_color[:,:,i], [clip_percent, \
-                                    100-clip_percent])
+                if clip_percent == 0:
+                    img_color_hist = [0,1]
+                else:
+                    img_color_hist = np.percentile(img_color[:,:,i], [clip_percent, 100-clip_percent])
                 img_color_clip[:,:,i] = (img_color[:,:,i]-img_color_hist[0])\
                                     /(img_color_hist[1]-img_color_hist[0]+0.0001)
         else:
-            img_color_hist = np.percentile(img_color, [clip_percent, 100-clip_percent])
+            if clip_percent == 0:
+                    img_color_hist = [0,1]
+            else:
+                img_color_hist = np.percentile(img_color, [clip_percent, 100-clip_percent])
             img_color_clip = (img_color-img_color_hist[0])\
-                                    /(img_color_hist[1]-img_color_hist[0]+0.0001)
-        img_color_clip = np.squeeze(img_color_clip)
+                                     /(img_color_hist[1]-img_color_hist[0]+0.0001)
         plt.imshow(np.clip(img_color_clip, 0, 1), extent=extent, vmin=0,vmax=1)
+
 
 def imsShow(img_list, img_name_list, clip_list=None, \
                                 color_bands_list=None, axis=None, row=None, col=None):
@@ -69,4 +71,4 @@ def imsShow(img_list, img_name_list, clip_list=None, \
             plt.title(img_name_list[ind])
             if not axis:
                 plt.axis('off')
-              
+
