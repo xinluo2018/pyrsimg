fc06eab89030d921a8ee0f35f17cd53a58f81402
Xin Luo <xinluo_xin@163.com>
2023-09-14 08:31:37 +0800

    new commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Xin Luo'
 author = 'Xin Luo'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.8'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -47,7 +47,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
