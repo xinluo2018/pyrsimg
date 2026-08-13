6a394d6820a2e3b77005d65e36474e70a06453ac
xin luo <xinluo_xin@163.com>
2026-08-13 15:34:37 +0800

    fix: remove missing metric_proc import

--- pyrsimg/__init__.py
+++ pyrsimg/__init__.py
@@ -3,7 +3,6 @@ from pyrsimg.img_normalize import *
 from pyrsimg.img2patch import *
 from pyrsimg.imgShow import *
 from pyrsimg.layer_stack import *
-from pyrsimg.metric_proc import *
 from pyrsimg.metrics import *
 from pyrsimg.raster_vec import *
 from pyrsimg.transform_time import *
